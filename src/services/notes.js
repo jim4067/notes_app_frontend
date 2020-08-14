613200fa97844d1cac1754acf4e807ccc80aea4f
jim4067 <jimmyimpulse2@gmail.com>
2020-08-14 23:06:49 +0300

    the user logged on can now create/post notes because the authorization header is now sent with the requests

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,20 +1,27 @@
 import axios from 'axios';
 const baseUrl = '/api/notes';
 
+let token = null;
+
+const setToken = newToken => {
+    token = `bearer ${newToken}`;
+}
+
 const getAll = () => {
     return axios.get(baseUrl);
 }
 
-const create = (newObject) => {
-    return axios.post(baseUrl, newObject);
+const create = async (newObject) => {
+    const config = {
+        headers : {Authorization : token}
+    }
+    const response = await axios.post(baseUrl, newObject, config);
+    return response.data;
 } 
 
 const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}` , newObject);
+    const request = axios.put(`${baseUrl}/${id}` , newObject);
+    return request;
 } 
 
-export default {
-    getAll : getAll,
-    create : create, 
-    update : update
-}
+export default {getAll, create, update, setToken}
