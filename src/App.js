96242a935648ee3810e8a6f434bb2abef1361bc8
jim4067 <jimmyimpulse2@gmail.com>
2020-08-18 15:52:31 +0300

    removed the note component from the App component

--- src/App.js
+++ src/App.js
@@ -106,19 +106,6 @@ const App = (props) => {
         }
     }
 
-    const NoteForm = () => {
-
-        return (
-            <div>
-                <form onSubmit={addNote}>
-                    <input value={newNote} onChange={handleNoteChange} />
-                    <button type='submit'>save</button>
-                </form>
-            </div>
-        );
-    }
-
-
     if (user === null) {
         return (
             <div>
