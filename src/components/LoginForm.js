89c92e5d0181fc9dd0ecceca11e9dc84f869e309
jim4067 <jimmyimpulse2@gmail.com>
2020-08-18 12:12:43 +0300

    finished the login form component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,1 +1,25 @@
 import React from 'react';
+
+const LoginForm = ({handleLogin, username, handleUsernameChange, password, handlePasswordChange,}) => {
+
+    return (
+        <form onSubmit={handleLogin} >
+            <div>
+                username <input
+                    name='username'
+                    onChange={handleUsernameChange}
+                    type='name'
+                    value={username} />
+            </div>
+            <div>
+                password <input
+                    name='password'
+                    onChange={handlePasswordChange}
+                    type='password'
+                    value={password} />
+            </div>
+            <button type='submit'>login</button>
+        </form>
+
+    );
+}
