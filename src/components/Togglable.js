14d10c9328404e54c027a2632b069d00b3104d43
jim4067 <jimmyimpulse2@gmail.com>
2020-08-18 15:01:52 +0300

    finished the togglable component:

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -10,10 +10,16 @@ const Togglable = (props) => {
     const toggleVisibility = () => {
         setVisible(!visible);
     }
-    
+
     return (
         <div>
-
+            <div style={hideWhenVisible}>
+                <button onClick={toggleVisibility}> {props.buttonLabel} </button>
+            </div>
+            <div style={showWhenVisible}>
+                {props.children}
+                <button onClick={toggleVisibility}> cancel </button>
+            </div>
         </div>
     );
 }
