a0f6d305ecd24c19ecf03ed4d6ebb93a29874ccc
jim4067 <jimmyimpulse2@gmail.com>
2020-08-18 12:05:10 +0300

    formatted the document

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,12 +3,12 @@ import React from "react";
 const Note = ({ note, toggleImportance }) => {
 
     const label = note.important ? "make not important" : "make important";
+    
     return (
         <li className="note">
             {note.content}
             <button onClick={toggleImportance}> {label} </button>
         </li>
-
     );
 };
 
