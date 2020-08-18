b0cb331fbbe37fbfba6e8f89885376e8c612bb6d
jim4067 <jimmyimpulse2@gmail.com>
2020-08-18 15:56:27 +0300

    exported the noteform

---
+++ src/components/NoteForm.js
@@ -0,0 +1,16 @@
+import React from 'reeact';
+
+
+const NoteForm = ({addNote, newNote, handleNoteChange}) => {
+
+    return (
+        <div>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type='submit'>save</button>
+            </form>
+        </div>
+    );
+}
+
+export default NoteForm;
