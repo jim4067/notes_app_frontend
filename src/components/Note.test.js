fa06c9a182c82dc21821869883cc7d6c7397b734
jim4067 <jimmyimpulse2@gmail.com>
2020-08-21 19:13:53 +0300

    :hammer: :hammer: refactored the code for note test

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -10,16 +10,19 @@ test("renders content", () => {
         important: true
     }
 
+
     const component = render(
         <Note note={note} />
     );
 
+    /*
     //searching for the li element
     const li = component.container.querySelector('li');
 
     console.log(prettyDOM(li));
+    */
 
-    /*
+
     //the different ways of investigating the contetns of the components being investigated
     //method 1
     expect(component.container).toHaveTextContent(
@@ -37,13 +40,12 @@ test("renders content", () => {
     expect(div).toHaveTextContent(
         "Component testing is done with react-tesing-library"
     );
-    */
 });
 
-test("clicking the button calls the event handler once" , () => {
+test("clicking the button calls the event handler once", () => {
     const note = {
-        content : "component testing is done with react-tesing-library",
-        important : true
+        content: "component testing is done with react-tesing-library",
+        important: true
     }
 
     const mockHandler = jest.fn();
