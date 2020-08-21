a05745a897283bf7681d2d4214591e1b59ae4435
jim4067 <jimmyimpulse2@gmail.com>
2020-08-21 12:39:14 +0300

    test verifies whether the component renders the content of the component

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import Render from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Note from './Note';
 
 test("renders content", () => {
     const note = {
-        content : "Component testing is done with react-tesing-library",
-        important : true
+        content: "Component testing is done with react-tesing-library",
+        important: true
     }
+
+    const component = render(
+        <Note note={note} />
+    );
+
+    expect(component.container).toHaveContent(
+        "Component testing is done with react-tesing-library"
+    );
 });
