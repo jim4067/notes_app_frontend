4f88823b219f47c17eec3392b9b7bf0e313e9235
jim4067 <jimmyimpulse2@gmail.com>
2020-08-21 18:58:13 +0300

    added the beforeEach function that gets called before each test

--- src/components/Togglable.test.js
+++ src/components/Togglable.test.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
-import './Togglable';
+import Togglable from './Togglable';
+
+describe("<Togglable />" , () => {
+    let component;
+
+    beforeEach(() => {
+        component = render(
+            <Togglable buttonLabel="show...">
+                <div className='testDiv' />
+            </Togglable>
+        );
+    });
+});
