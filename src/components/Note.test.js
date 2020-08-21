5984b723c75ebc8507960cca3201e9a7c7f441f5
jim4067 <jimmyimpulse2@gmail.com>
2020-08-21 14:52:06 +0300

    :hammer: :hammer: formatted the code

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import {prettyDOM} from '@testing-library/dom';
+import { prettyDOM } from '@testing-library/dom';
 import Note from './Note';
 
 test("renders content", () => {
