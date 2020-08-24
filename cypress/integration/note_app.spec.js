a8fad0d994dc341091e58c2dc42e80f48799c572
jim4067 <jimmyimpulse2@gmail.com>
2020-08-24 16:34:37 +0300

    the first E2E test

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,7 +1,7 @@
 describe('Note app', function() {
     it("front page can be opnened", function() {
-        cy.visit('http://localhost:3000');
-        cy.contains("login");
+        window.cy.visit('http://localhost:3000');
+        window.cy.contains("login");
         //cy.contains("Note app, Department of Physical Sciences, University of Nairobi 2020");
     });
 });
