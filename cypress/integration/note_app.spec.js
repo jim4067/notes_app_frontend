d2434bfb2423bad06a04ee54dd7e4ed301090c63
jim4067 <jimmyimpulse2@gmail.com>
2020-08-24 16:43:59 +0300

    removed redundant code and added a beforeEach function

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,12 +1,14 @@
 describe('Note app', function() {
-    it("front page can be opnened", function() {
+    beforeEach(function () {
         window.cy.visit('http://localhost:3000');
+    });
+
+    it("front page can be opnened", function() {
         window.cy.contains("login");
         //cy.contains("Note app, Department of Physical Sciences, University of Nairobi 2020");
     });
 
     it("login form can be opnened", function() {
-        window.cy.visit('http://localhost:3000');
         window.cy.contains('login').click();
     })
 });
