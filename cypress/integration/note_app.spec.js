5c482b5306353510de6eeb110d2f5ca813228c7c
jim4067 <jimmyimpulse2@gmail.com>
2020-08-24 17:34:23 +0300

    the test for creating a new note passed

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -14,5 +14,19 @@ describe('Note app', function() {
         cy.get('#login-button').click();
 
         cy.contains('Note app, Department of Physical Sciences, University of Nairobi 2020');
-    })
+    });
+
+    it("a new note can be added", function() {
+        cy.contains('login').click();
+        cy.get('#username').type("jim4067");
+        cy.get('#password').type("pass123");
+        cy.get('#login-button').click();
+
+        cy.contains('new note').click();
+        cy.get('input').type("a note created by cypress");
+        cy.contains('save').click();
+        cy.contains("a note created by cypress");
+    });
 });
+
+//don't forget to add the cypress plugin
