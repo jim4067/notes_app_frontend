f275f7cbb5e11f8a4639f80d89deed58f506e7d1
jim4067 <jimmyimpulse2@gmail.com>
2020-08-24 17:12:14 +0300

    stopped using the window for global variable cy

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,18 +1,18 @@
 describe('Note app', function() {
     beforeEach(function () {
-        window.cy.visit('http://localhost:3000');
+        cy.visit('http://localhost:3000');
     });
 
     it("front page can be opnened", function() {
-        window.cy.contains("login");
+        cy.contains("login");
     });
 
     it("login form can be opnened and user log in", function() {
-        window.cy.contains('login').click();
-        window.cy.get('#username').type("jim4067");
-        window.cy.get('#password').type("pass123");
-        window.cy.get('#login-button').click();
+        cy.contains('login').click();
+        cy.get('#username').type("jim4067");
+        cy.get('#password').type("pass123");
+        cy.get('#login-button').click();
 
-        window.cy.contains('Note app, Department of Physical Sciences, University of Nairobi 2020');
+        cy.contains('Note app, Department of Physical Sciences, University of Nairobi 2020');
     })
 });
