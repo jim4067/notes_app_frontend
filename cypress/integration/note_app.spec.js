4a6e7317a3638e8db47ff23335b84a711a54f1cf
jim4067 <jimmyimpulse2@gmail.com>
2020-08-28 18:30:48 +0300

    added a cypress command for logging in so that we will not repeat the code everywhere

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -26,59 +26,49 @@ describe('Note app', function () {
     describe("when logged in ", function () {
 
         beforeEach(function () {
-            const user_to_login = {
-                username : "jim4067",
-                password : "pass123"
-            }
+            cy.login({ username: "jim4067", password: "pass123" })
 
-            cy.request('POST', 'http://localhost:3040/api/login', user_to_login)
-              .then(function (response){
-                  localStorage.setItem('loggedNoteAppUser', JSON.stringify(response.body))
-                  cy.visit('http://localhost:3000')
-              })
+            it.only("a new note can be added and its importance changed", function () {
+                cy.contains('new note').click();
+                cy.get('input').type("created by cypress");
+                cy.contains('save').click();
+                cy.contains("created by cypress")
+                    .contains('make important')
+                    .click();
+
+                cy.contains("created by cypress")
+                    .contains('make not important')
+            });
+
+            /*slow net. test timeout before this is tested
+            it("and its importance can be changed", function () {
+                cy.contains('created by cypress')
+                    .contains('make important')
+                    .click();
+    
+                cy.contains("created by cypress")
+                    .contains('make not important');
+            });
+            */
         });
 
-        it.only("a new note can be added and its importance changed", function () {
-            cy.contains('new note').click();
-            cy.get('input').type("created by cypress");
-            cy.contains('save').click();
-            cy.contains("created by cypress")
-                .contains('make important')
-                .click();
+        describe("password failure ", function () {
+            //Testing only one test using .only 
+            it("login fails with wrong password", function () {
+                cy.contains("login").click();
+                cy.get('#username').type("jim47");
+                cy.get('#password').type("this is wrong");
+                cy.get('#login-button').click();
 
-            cy.contains("created by cypress")
-                .contains('make not important')
+                cy.get('.error')
+                    .should('contain', "wrong credentials")
+                    .and('have.css', 'color', 'rgb(255, 0, 0)')
+                    .and('have.css', 'border-style', 'solid')
+
+                cy.get('html').should('not.contain', "James Mutuku logged in");
+            });
         });
 
-        /*slow net. test timeout before this is tested
-        it("and its importance can be changed", function () {
-            cy.contains('created by cypress')
-                .contains('make important')
-                .click();
-
-            cy.contains("created by cypress")
-                .contains('make not important');
-        });
-        */
     });
-
-    describe("password failure ", function () {
-        //Testing only one test using .only 
-        it("login fails with wrong password", function () {
-            cy.contains("login").click();
-            cy.get('#username').type("jim47");
-            cy.get('#password').type("this is wrong");
-            cy.get('#login-button').click();
-
-            cy.get('.error')
-                .should('contain', "wrong credentials")
-                .and('have.css', 'color', 'rgb(255, 0, 0)')
-                .and('have.css', 'border-style', 'solid')
-
-            cy.get('html').should('not.contain', "James Mutuku logged in");
-        });
-    });
-
-});
 
 //don't forget to add the cypress plugin
